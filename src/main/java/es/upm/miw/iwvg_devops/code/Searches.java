e23291e6397a1f18fa0fbb9c4cc213b83adbf95f
Pablo <pabloguijarro96@gmail.com>
2020-09-20 11:08:10 +0200

    First search+Test added #10

---
+++ src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
@@ -0,0 +1,41 @@
+package es.upm.miw.iwvg_devops.code;
+
+import org.apache.logging.log4j.LogManager;
+
+import java.util.stream.Stream;
+
+public class Searches {
+
+    public Stream<String> findUserFamilyNameByUserNameDistinct(String userName) {
+        return new UsersDatabase().findAll()
+                .filter(user -> userName.equals(user.getName()))
+                .map(User::getFamilyName)
+                .distinct();
+    }
+
+    public Stream<Integer> findFractionNumeratorByUserFamilyName(String userFamilyName) {
+        return new UsersDatabase().findAll()
+                .peek(x -> LogManager.getLogger(this.getClass()).info("before: " + x))
+                .filter(user -> userFamilyName.equals(user.getFamilyName()))
+                .peek(x -> LogManager.getLogger(this.getClass()).info("after: " + x))
+                .flatMap(user -> user.getFractions().stream())
+                .map(Fraction::getNumerator);
+    }
+
+    public Stream<String> findUserFamilyNameByFractionDenominator(int fractionDenominator) {
+        return new UsersDatabase().findAll()
+                .filter(user -> user.getFractions().stream()
+                        .anyMatch(fraction -> fractionDenominator == fraction.getDenominator()))
+                .map(User::getFamilyName);
+    }
+
+    public Fraction findFractionAdditionByUserId(String id) {
+        return new UsersDatabase().findAll()
+                .filter(user -> user.getId().equals(id))
+                .map(user -> user.getFractions())
+                .flatMap(fractions -> fractions.stream())
+                .reduce((fraction, fraction2) -> fraction2.add(fraction, fraction2))
+                .get();
+    }
+
+}
---
+++ src/test/java/es/upm/miw/iwvg_devops/rest/code/SearchesTest.java
@@ -0,0 +1,42 @@
+package es.upm.miw.iwvg_devops.rest.code;
+
+import es.upm.miw.iwvg_devops.code.Fraction;
+import es.upm.miw.iwvg_devops.code.Searches;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SearchesTest {
+
+    @Test
+    void testFindUserFamilyNameByUserNameDistinct() {
+        assertEquals(List.of("Torres"), new Searches().findUserFamilyNameByUserNameDistinct("Paula")
+                .collect(Collectors.toList()));
+    }
+
+    @Test
+    void testFindUserFractionNumeratorByFamilyName() {
+        assertEquals(List.of(2, 4, 0, 1, 1), new Searches().findFractionNumeratorByUserFamilyName("Torres")
+                .collect(Collectors.toList()));
+    }
+
+    @Test
+    void testFindFamilyNameByFractionDenominator() {
+        assertEquals(List.of("López", "Torres"), new Searches().findUserFamilyNameByFractionDenominator(2)
+                .collect(Collectors.toList()));
+    }
+
+    @Test
+    void testFindFractionAdditionByUserId() {
+        Fraction fraction = new Searches().findFractionAdditionByUserId("1");
+        assertEquals("Fraction{" +
+                        "numerator=" + 3 +
+                        ", denominator=" + 1 + "}",
+                "Fraction{" + "numerator=" +
+                        fraction.getNumerator() +
+                        ", denominator=" + fraction.getDenominator() + "}");
+    }
+}
